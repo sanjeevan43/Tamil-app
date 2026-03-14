a3c33695ec2410228f486610b7e089d5e8011db0
Sanjeevan43 <sanjeevamoorthy35@gmail.com>
2026-03-14 15:40:24 +0530

    hii

--- admin_panel/src/firebase.ts
+++ admin_panel/src/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDw-pCDEHyBs2lPnpVsLZXD1i6hh65L95k",
-    appId: "1:356453132420:web:256c9c8127289bcc96bad1",
-    messagingSenderId: "356453132420",
-    projectId: "h3-tamil-app",
-    authDomain: "h3-tamil-app.firebaseapp.com",
-    storageBucket: "h3-tamil-app.firebasestorage.app",
-    measurementId: "G-XFC0SQ0ZTJ"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
